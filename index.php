1f4a560070f70146e1633365f6ec52fdb100c9af
pedro <2015210595@udh.edu.pe>
2022-07-01 12:32:31 -0500

    comienzo del proyecto

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<html lang="es">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Sistema de Gobierno Regional</title>
-  </head>
-<?php
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>sistema de gestion para el turismo en la region Huanuco</title>
+      <link rel="stylesheet" href="stilos.css">
+      
+</head>
+<body>
+      <div class="cabecera">
+            <div class="imagen">
+            <img src="imagenes/logo.png" alt="" width="80px" height="80px">
+            </div>
+            <div class="texto">
+            <h3>Bienvenido a la ciudad de Huanuco </h3>
+            </div>
+      </div>
+        <div class="menu_nav">
+            <ul>
+                  <li><a href="#">Hoteles</a></li>
+                  <li><a href="#">Restaurants</a></li>
+                  <li><a href="#">Destinos turisticos</a></li>
+            </ul>
 
-
-?>
+      </div>
+</body>
+</html>
