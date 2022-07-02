495e55e371696a66354347f68651c93ba027137c
MOISES <90525345+moisses21@users.noreply.github.com>
2022-07-02 12:28:46 -0500

    Update login.php

--- login.php
+++ login.php
@@ -26,8 +26,8 @@
 
 <?php
 if(!empty($_POST)){
-    $administradorControlador = new AdministradorControlador();
-    $administradorControlador->login($_POST["username"],$_POST["password"]);
+    $administrador_Controlador = new AdministradorControlador();
+    $administrador_Controlador->login($_POST["username"],$_POST["password"]);
 }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
